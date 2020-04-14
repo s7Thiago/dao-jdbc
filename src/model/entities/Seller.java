97127f3463a1a600be8a299ccae38da1490c740e
s7thiago <thyagosousasilva@gmail.com>
2020-04-14 20:31:43 -0300

    Implementing Seller.java class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 public class Seller implements Serializable {
 
-    private static final long serianVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private Integer id;
     private String name;
